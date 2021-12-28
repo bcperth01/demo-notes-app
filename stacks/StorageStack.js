d355a214d4c3a90c086cb6ae65033440ce1cffce
brendan <brendan@bcperth.com>
2021-12-28 21:04:55 +0800

    Added bucket

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,21 +1,26 @@
-// ceaet a storage stack for Dynamo tables and 
+// ceaet a storage stack for Dynamo tables and S3 buckets
 import * as sst from "@serverless-stack/resources";
 
 export default class StorageStack extends sst.Stack {
+  bucket;
   table;
 
   constructor(scope, id, props) {
     super(scope, id, props);
+
+    // add a bucket
+    this.bucket = new sst.Bucket(this, "Uploads");
+
+    // add a DynamoDB Table
+
     this.table = new sst.Table(this, "Notes", {
       fields: {
         userId: sst.TableFieldType.STRING,
         noteId: sst.TableFieldType.STRING,
       },
       primaryIndex: {
-        partitionIndex: {
-          partitionKey: "userId",
-          sortKey: "NoteId",
-        },
+        partitionKey: "userId",
+        sortKey: "noteId",
       },
     });
   }
